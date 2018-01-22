9fc7d09a25a5d4bc99f738783ae6767109cc363f
OlgaOdintsova <olya.odintsova@gmail.com>
2018-01-22 14:08:16 +0300

    add homework 2(ex2)

--- src/test/java/homework2/RegressTest.java
+++ src/test/java/homework2/RegressTest.java
@@ -7,16 +7,25 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
-public class SmokeTest extends TestBase {
+public class RegressTest extends TestBase {
 
     // Assert Browser title
-    @Test(groups = {"smoke"})
+    @Test(groups = {"regress"})
     public void testPageTitle() {
         assertEquals(driver.getTitle(), "Index Page");
     }
 
+    // Perform login
+    @Test(groups = {"regress"})
+    public void testLogin() {
+        driver.findElement(By.cssSelector(".uui-profile-menu")).click();
+        driver.findElement(By.id("Login")).sendKeys("epam");
+        driver.findElement(By.id("Password")).sendKeys("1234");
+        driver.findElement(By.cssSelector(".fa-sign-in")).click();
+    }
+
     //Assert that the main header is displayed
-    @Test(groups = {"smoke"})
+    @Test(groups = {"regress"})
     public void testDisplayedTitle() {
         assertTrue(driver.findElement(By.cssSelector(".main-title")).isDisplayed());
     }
---
+++ src/test/java/homework2/SmokeAndRegressTest.java
@@ -0,0 +1,41 @@
+package homework2;
+
+import homework.base.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class SmokeAndRegressTest extends TestBase {
+    // Assert Browser title
+    @Test(groups = {"smoke"})
+    public void testPageTitle() {
+        assertEquals(driver.getTitle(), "Index Page");
+    }
+
+    //Assert that the main header is displayed
+    @Test(groups = {"smoke"})
+    public void testDisplayedTitle() {
+        assertTrue(driver.findElement(By.cssSelector(".main-title")).isDisplayed());
+    }
+
+    // Assert Browser title
+    @Test(groups = {"regress"})
+    public void testRegressPageTitle() {
+        assertEquals(driver.getTitle(), "Index Page");
+    }
+
+    //7 Assert that there are 4 images on the Home Page and they are displayed
+    @Test(groups = {"smoke","regresss"})
+    public void testImages() {
+        List<WebElement> images = driver.findElements(By.className("benefit-icon"));
+        assertEquals(images.size(), 4);
+        for (WebElement icon : images) {
+            assertTrue(icon.isDisplayed());
+        }
+    }
+}
