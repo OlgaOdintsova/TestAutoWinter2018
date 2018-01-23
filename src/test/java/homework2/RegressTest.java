66745b8c16bddf126bc8b6d2eb5e92586bb8f802
OlgaOdintsova <olya.odintsova@gmail.com>
2018-01-23 08:40:22 +0300

    add homework 2(ex2)

--- src/test/java/homework2/RegressTest.java
+++ src/test/java/homework2/RegressTest.java
@@ -1,31 +1,14 @@
 package homework2;
 
 
+import homework.base.TestBase;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
-public class RegressTest {
-
-    private static WebDriver driver;
-
-    @BeforeSuite(alwaysRun = true)
-    public void setUp() {
-        driver = new ChromeDriver();
-        driver.navigate().to("https://jdi-framework.github.io/tests");
-        driver.manage().window().maximize();
-    }
-
-    @AfterSuite(alwaysRun = true)
-    public void tearDown() {
-        driver.close();
-    }
+public class RegressTest extends TestBase {
 
     // Assert Browser title
     @Test(groups = {"regress"})
@@ -47,5 +30,4 @@ public class RegressTest {
     public void testDisplayedTitle() {
         assertTrue(driver.findElement(By.cssSelector(".main-title")).isDisplayed());
     }
-
 }
--- src/test/java/homework2/SmokeAndRegressTest.java
+++ src/test/java/homework2/SmokeAndRegressTest.java
@@ -2,11 +2,7 @@ package homework2;
 
 import homework.base.TestBase;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -14,21 +10,7 @@ import java.util.List;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
-public class SmokeAndRegressTest {
-
-    private static WebDriver driver;
-
-    @BeforeSuite(alwaysRun = true)
-    public void setUp() {
-        driver = new ChromeDriver();
-        driver.navigate().to("https://jdi-framework.github.io/tests");
-        driver.manage().window().maximize();
-    }
-
-    @AfterSuite(alwaysRun = true)
-    public void tearDown() {
-        driver.close();
-    }
+public class SmokeAndRegressTest extends TestBase {
 
     // Assert Browser title
     @Test(groups = {"smoke"})
